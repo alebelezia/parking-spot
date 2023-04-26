5ff39e0dd67e9d8d4cfd03d3c26781738e9bac0c
Alessandra Belézia <alebelezia@gmail.com>
2023-04-26 10:32:41 -0300

    Criação do método DELETE

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -50,4 +50,9 @@ public class ParkingSpotService {
         return parkingSpotRepository.findById(id);
     }
 
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
+    }
+
 }
