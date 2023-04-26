51c59c77fcb54f1e61eaaf35bec3b0513e2bd1e7
Alessandra Belézia <alebelezia@gmail.com>
2023-04-26 10:27:26 -0300

    Criação do método GET one

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.api.parkingcontrol.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -42,6 +44,10 @@ public class ParkingSpotService {
 
     public List<ParkingSpotModel> findAll() {
         return parkingSpotRepository.findAll();
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
     }
 
 }
